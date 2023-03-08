885e09be20285bce545d2c47d1713f5a255eff1a
Noah Hicks <noahg@ad.unc.edu>
2023-03-08 18:56:02 -0500

    uodates

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,24 +1,28 @@
+#!/usr/bin/env node
+
 import minimist from "minimist";
 import { rps } from "../lib/rpsls";
 
 let argv = minimist(process.argv.slice(2))
 
-const help = `Usage: node-rps [SHOT]
+const help = `
+Usage: node-rps [SHOT]
 Play Rock Paper Scissors (RPS)
 
-  -h, --help      display this help message and exit
-  -r, --rules     display the rules and exit
+    -h, --help      display this help message and exit
+    -r, --rules     display the rules and exit
 
 Examples:
-  node-rps        Return JSON with single player RPS result.
-                  e.g. {"player":"rock"}
-  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                  e.g {"player":"rock","opponent":"scissors","result":"win"}`
+    node-rps        Return JSON with single player RPS result.
+                    e.g. {"player":"rock"}
+    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"scissors","result":"win"}`;
 
-const rules = `Rules for Rock Paper Scissors:
-                      - Scissors CUTS Paper
-                      - Paper COVERS Rock
-                      - Rock CRUSHES Scissors`;
+const rules = `
+Rules for Rock Paper Scissors:
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`;
 
 
 //If statement to show help
